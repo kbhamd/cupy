0d314faca4c8fcb04943d94b91fb8fef9039a5cb
kbhamd <kjetil.haugen@amd.com>
2024-09-08 22:06:21 -0500

    build: define CUB_PATH environment variable

--- buildme.sh
+++ buildme.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+#HIPCC=`readlink $GUIX_ENVIRONMENT/bin/hipcc`
+#HIPCUB=`readlink $GUIX_ENVIRONMENT/include/hipcub`
 
 export CUPY_INSTALL_USE_HIP=1
 export HCC_AMDGPU_TARGET=gfx90a,gfx942,gfx1030
+export ROCM_HOME=$GUIX_ENVIRONMENT
 
-HIPCC=`readlink $GUIX_ENVIRONMENT/bin/hipcc`
-export ROCM_HOME=${HIPCC%%/bin/hipcc}
-export CUB_INCLUDE_PATH=`readlink $GUIX_ENVIRONMENT/include/hipcub`
+#export ROCM_HOME=${HIPCC%%/bin/hipcc}
+#export CUB_PATH=${HIPCUB%%/include/hipcub}
 
-export CFLAGS="-I$GUIX_ENVIRONMENT/include"
+#export CFLAGS="-I$GUIX_ENVIRONMENT/include"
 
 export CPLUS_INCLUDE_PATH=$GUIX_ENVIRONMENT/include
 
